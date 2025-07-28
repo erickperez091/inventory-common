6031ce76b0db18adcd8d0038c0f52e46eb703941
Erick <erickperez091@hotmail.com>
2025-07-28 11:46:43 -0600

    Adding Global security to share across microservices

--- src/main/java/com/example/common/security/GlobalSecurityConfig.java
+++ src/main/java/com/example/common/security/GlobalSecurityConfig.java
@@ -1,33 +1,56 @@
 package com.example.common.security;
 
+import com.example.common.utilities.JwtUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-//@EnableWebSecurity
-//@EnableMethodSecurity
+import java.util.List;
+
+@Configuration
 @RequiredArgsConstructor
+@EnableMethodSecurity
+@ConditionalOnProperty(name = "security.enabled", havingValue = "true", matchIfMissing = true)
 public class GlobalSecurityConfig {
 
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    @Value("#{'${security.public.url:}'.split(',')}")
+    private List<String> publicUrls;
 
+    private final JwtUtils jwtUtils;
 
-    //@Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers("/public/**").permitAll()
-                        .requestMatchers("/auth/v1/**").permitAll()
-                        .requestMatchers("**/v1/ping", "/actuator/**").permitAll()
-                        .anyRequest().authenticated())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(jwtUtils);
+    }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "security.enabled", havingValue = "true")
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    if (publicUrls != null && !publicUrls.isEmpty()) {
+                        publicUrls.forEach(url -> {
+                            auth.requestMatchers(url).permitAll();
+                        });
+                    }
+                    auth.anyRequest().authenticated();
+                })
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
